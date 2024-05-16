d96aa7d0f1d27f0130dec4a035730d84b0d80210
imooon <imonidharia@gmail.com>
2024-05-16 00:11:49 -0400

    preparing to add projects

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,16 @@
+// creation of 'Projects' boiler plate through rfce
+import styles from './ProjectStyles.module.css';
+
+function Projects() {
+  return (
+    <section id='projects' className={styles.container}>
+    <h1 className='sectionTitle'>Projects</h1>
+    <div className={styles.projectContainer}>
+        <a href=""></a>
+    </div>
+
+    </section>
+  )
+}
+
+export default Projects
