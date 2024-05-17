5567cef2cc07242554ee73e0ff6b96c56decc67c
imooon <imonidharia@gmail.com>
2024-05-16 23:52:09 -0400

    added skills

---
+++ src/sections/Skills/Skills.jsx
@@ -0,0 +1,30 @@
+import styles from './SkillsStyles.module.css';
+import checkMarkIcon from '../../assets/checkmark-dark.svg';
+import SkillList from '../../common/SkillList';
+
+function Skills() {
+  return (
+    <section id="skills" className={styles.container}>
+      <h1 className="sectionTitle">Skills</h1>
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="HTML" />
+        <SkillList src={checkMarkIcon} skill="CSS" />
+        <SkillList src={checkMarkIcon} skill="JavaScript" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Node" />
+        <SkillList src={checkMarkIcon} skill="Express" />
+        <SkillList src={checkMarkIcon} skill="SQL" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Git" />
+        <SkillList src={checkMarkIcon} skill="React" />
+        <SkillList src={checkMarkIcon} skill="MERN" />
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
