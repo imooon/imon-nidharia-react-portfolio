fdd3ccfd2ae82de8623043fa263f640f81148d85
imooon <imonidharia@gmail.com>
2024-05-16 23:24:02 -0400

    added group projects

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,16 +1,21 @@
 // creation of 'Projects' boiler plate through rfce
 import styles from './ProjectStyles.module.css';
+import viberr from '../../assets/viberr.png';
+import ProjectCard from '../../common/ProjectCard';
+import freshBurger from '../../assets/fresh-burger.png';
 
 function Projects() {
   return (
     <section id='projects' className={styles.container}>
-    <h1 className='sectionTitle'>Projects</h1>
-    <div className={styles.projectContainer}>
-        <a href=""></a>
+    <h1 className='sectionTitle'>My Projects</h1>
+    <div className={styles.projectsContainer}>
+      <ProjectCard src={viberr} link="https://github.com/imooon/Harmony-Hub-App" h3="Harmony Hub" p="A feel-good wellness application!"/>
+
+      <ProjectCard src={freshBurger} link="https://github.com/imooon/motormatcher-app" h3="Motor Matcher" p="Buy & Sell cars!"/>
     </div>
 
     </section>
   )
 }
 
-export default Projects
+export default Projects;
