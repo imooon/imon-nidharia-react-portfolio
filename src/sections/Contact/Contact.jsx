046fb6c10e4dd58f104a8160bb2482787ebf5065
Imon Nidharia <imonidharia@gmail.com>
2024-06-25 14:32:29 -0400

    functional form

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -3,7 +3,7 @@ function Contact() {
   return (
     <section id="contact" className={styles.container}>
         <h1 className="sectionTitle">Reach out!</h1>
-        <form action="">
+        <form action="https://formspree.io/f/xwpeeakn" method="post">
             <div className="formGroup">
                 <label htmlFor="name" hidden>
                     Name
