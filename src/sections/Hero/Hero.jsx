a4a25c59a995d890f7063ef074b30c0a2b860854
Imon Nidharia <imonidharia@gmail.com>
2024-06-25 12:21:38 -0400

    changed dp to pixel art

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from './HeroStyles.module.css';
-import heroImg from '../../assets/hero-img.png';
+import profilepicture from '../../assets/profilepicture-modified.png';
 import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
 import githubLight from '../../assets/github-light.svg';
@@ -23,7 +23,7 @@ import { useTheme } from '../../common/ThemeContext';
         return <section id="hero" className={styles.container}>
             <div className={styles.colorModeContainer}>
                 <img className={styles.hero}
-                     src={heroImg} alt="Profile picture of Imon Nidharia" 
+                     src={profilepicture} alt="Profile picture of Imon Nidharia" 
                      />
                      <img className={styles.colorMode} src={themeIcon} alt="Colour mode icon" onClick={toggleTheme}/>
             </div> 
