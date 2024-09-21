05b6872d48260332545aa4b9b19dd4c14c323da2
Imon Nidharia <imonidharia@gmail.com>
2024-09-20 23:08:48 -0400

    feat: added basic animation to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
-import './Navbar.css'; // Make sure you have this CSS file for styling
+import React, { useState, useEffect } from 'react';
+import '../Navbar/Navbar.css';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoaded(true);
+    }, 500); // Delay for the animation to start
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <nav className="navbar">
-      <div className="navbar-left">
-        <h1>Imon Nidharia</h1>
+      <div className={`navbar-left ${loaded ? 'fade-in' : ''}`}>
+        Imon Nidharia
       </div>
-      <div className="navbar-right">
-        <a href="#about">About Me</a>       
-        <a href="#home">Home</a>
-        <a href="#projects">Projects</a>
-        <a href="#contact">Contact</a>
+      <div className={`navbar-right ${isOpen ? 'open' : ''} ${loaded ? 'fade-in-links' : ''}`}>
+        <a href="#about" className="coolBeans">About Me</a>
+        <a href="#home" className="coolBeans">Home</a>
+        <a href="#projects" className="coolBeans">Projects</a>
+        <a href="#contact" className="coolBeans">Contact</a>
+      </div>
+      <div className="hamburger" id="hamburger-menu" onClick={toggleNavbar}>
+        &#9776; {/* Hamburger icon */}
       </div>
     </nav>
   );
